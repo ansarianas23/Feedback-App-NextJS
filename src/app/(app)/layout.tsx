ac52b0cc2c52203c7e5edaef74445cedf929d468
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-01 23:29:31 +0530

    "few code refactoring"

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
